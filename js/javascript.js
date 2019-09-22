41a1091e0f18ab9ad9145a382f94bae5c0b54d23
Sam <samanta493@gmail.com>
2019-09-22 18:19:29 -0500

    Banner principal terminado

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,39 @@
 
-$('.slider').slick({
+$('.slider-principal').slick({
   dots: true,
   infinite: true,
   speed: 100,
   slidesToShow: 1,
   adaptiveHeight: true,
-
+  autoplay: true,
+  autoplaySpeed: 3000,
+  fade: true,
+  cssEase: 'linear'
 });
+
+
+$('.slider-catalogo').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 2000
+});
+
+
+/*
+  Slidemenu
+*/
+(function() {
+	var $body = document.body
+	, $menu_trigger = $body.getElementsByClassName('menu-trigger')[0];
+
+	if ( typeof $menu_trigger !== 'undefined' ) {
+		$menu_trigger.addEventListener('click', function() {
+			$body.className = ( $body.className == 'menu-active' )? '' : 'menu-active';
+		});
+	}
+
+}).call(this);
